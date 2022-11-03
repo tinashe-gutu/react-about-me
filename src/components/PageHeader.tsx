b89cbe0428abf0e7a76fdc7a21014b927998e19f
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-11-03 12:08:56 +0000

    change header title

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,7 +1,7 @@
 function PageHeader(): JSX.Element {
   return (
     <header>
-      <h1>Ye Olde Web Page</h1>
+      <h1>Perfect Web Page</h1>
       <p>Welcome to Nichard's Ye Olde Web Page!</p>
       <p>
         This shows a random scatter of my thoughts, last updated on March 4,
